d3724c26a4dc98883625d04f0161b2638b7ddc94
Alisson Medeiros de Melo <alissonmedeirosdemelo@alisson.medeiros>
2023-11-19 00:53:31 -0300

    first commit

---
+++ src/components/CardGame/index.js
@@ -0,0 +1,10 @@
+import './style.css'
+
+export default function CardGame(srcImage) {
+    return `
+        <article class="card-game">
+            <img src="images/alura-pixel.png" alt="">
+        </article>
+    `
+}
+
