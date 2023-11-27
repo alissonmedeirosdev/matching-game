89887e444acf013a5e787c400e6363e707b8f3c9
Alisson Medeiros de Melo <alissonmedeirosdemelo@alisson.medeiros>
2023-11-26 23:31:30 -0300

    work in progress

--- main.js
+++ main.js
@@ -3,11 +3,12 @@ import './src/styles/settings/colors.css'
 import './src/styles/elements/base.css'
 import BoardGame from './src/objects/BoardGame'
 import ScoreBoard from './src/objects/ScoreBoard'
+// import CardFrontBack from './src/components/CardFrontBack'
 
 const $root = document.querySelector('#root')
 
 
 $root.insertAdjacentHTML('beforeend', `  
     ${ScoreBoard()}
-    ${BoardGame(6)}
+    ${BoardGame(2)}
 `)
--- src/objects/BoardGame/index.js
+++ src/objects/BoardGame/index.js
@@ -1,9 +1,10 @@
 import './style.css'
-import CardGame from '../../components/CardGame'
+// import CardGame from '../../components/CardGame'
+import CardFrontBack from '../../components/CardFrontBack'
 
 export default function BoradGame(amountCard) {
-    const $htmlCardGame = CardGame() 
-    const $htmlContent = $htmlCardGame.repeat(amountCard)
+    const $htmlCardFrontBack = CardFrontBack() 
+    const $htmlContent = $htmlCardFrontBack.repeat(amountCard)
 
     return `
         <section class="board-game">
