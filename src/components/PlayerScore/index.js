d6ad2c3ead42cab1c48f3c694b66c315a65d8df5
Alisson Medeiros de Melo <alissonmedeirosdemelo@alisson.medeiros>
2023-12-06 01:52:52 -0300

    Add or update the UI and style files

---
+++ src/components/PlayerScore/index.js
@@ -0,0 +1,9 @@
+export default function PlayerScore(points = 0){
+    return /*html*/`
+        <ol data-point="${points}">
+            <li>Um</li>
+            <li>Dois</li>
+            <li>Três</li>
+        </ol>
+    `
+}
--- src/objects/ScoreBoard/index.js
+++ src/objects/ScoreBoard/index.js
@@ -1,3 +1,4 @@
+import PlayerScore from '../../components/PlayerScore'
 import VsPalyer from '../../components/VsPlayer'
 import PlayerName from './../../components/PlayerName'
 import './style.css'
@@ -6,7 +7,9 @@ export default function ScoreBoard() {
     return /*html*/`
         <header class="score-board">
             ${PlayerName('Player 1')}
+            ${PlayerScore(3)}
             ${VsPalyer()}
+            ${PlayerScore(3)}
             ${PlayerName('Player 2')}
         </header>
         
